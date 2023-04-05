87adf28718225ed54115ebb96f08693deb9ab47f
Ahmed Nader <ah00355@tkh.edu.eg>
2023-04-06 00:07:53 +0200

    added comments

--- final-rainbowgen-hashtables.py
+++ final-rainbowgen-hashtables.py
@@ -1,20 +1,25 @@
+# Importing necessary libraries
 import hashlib
 import random
 import string
 
+# Defining a node class
 class Node:
     def __init__(self, key, value=None):
-        self.key = key
-        self.value = value
+        self.key = key  # The key to be stored
+        self.value = value  # The value to be stored
 
+# Function to insert data into the hash table
 def insert(table, key, value):
     table[key] = Node(key, value)
 
+# Asking user to choose a hashing algorithm from the following
 print("Choose a hashing algorithm from the following:")
 print("1. MD5")
 print("2. SHA1")
 print("3. SHA256")
 
+# Continuously asking for user input until a valid choice is entered
 while True:
     choice = input("Please enter your choice of the hashing algorithm: ")
     if choice == '1':
@@ -27,8 +32,9 @@ while True:
         algorithm = hashlib.sha256
         break
     else:
-        print("Invalid choice. Please enter a valid choice (1, 2, or 3).")    
+        print("Invalid choice. Please enter a valid choice (1, 2, or 3).")
 
+# Generating 1000 random passwords and hashing them using the chosen algorithm
 passwords = []
 print("Generating passwords...")
 for i in range(1000):
@@ -37,7 +43,8 @@ for i in range(1000):
 hashes = [algorithm(password.encode('utf-8')).hexdigest() for password in passwords]
 print("Hashing passwords...")
 
-def reduce(hash_string: str, iteration: int, alphabet= string.ascii_letters + string.digits + string.punctuation, word_length: int = 8) -> str:
+# Function to reduce a hash string to a password using an iteration and an alphabet
+def reduce(hash_string: str, iteration: int, alphabet=string.ascii_letters + string.digits + string.punctuation, word_length: int = 8) -> str:
     if alphabet is None:
         alphabet = list(string.ascii_letters + string.digits + string.punctuation)
 
@@ -49,8 +56,10 @@ def reduce(hash_string: str, iteration: int, alphabet= string.ascii_letters + st
         result.append(alphabet[mod])
     return "".join(result)
 
+# Setting the chain length for the rainbow table
 chain_length = 1000
 
+# Creating the rainbow table
 hash_table = {}
 for i in range(len(passwords)):
     password = passwords[i]
@@ -60,6 +69,7 @@ for i in range(len(passwords)):
         hash_val = algorithm(password.encode('utf-8')).hexdigest()
     insert(hash_table, hash_val, password)
 
+# Printing the rainbow table
 print("Rainbow table:")
 print("{:<10} {:<34} {:<10}".format("Password", "Last value in the chain", "Hash"))
 print("-" * 70)
@@ -68,6 +78,7 @@ for key in hash_table:
     node = hash_table[key]
     print("{:<10} {:<34} {:<10}".format(node.value, reduce(node.key, chain_length - 1), node.key))
 
+# Asking user to enter a hash value to find the original password
 hash_val_to_find = input("Enter the hash value to find the original password: ")
 password_node = hash_table.get(hash_val_to_find)
 if password_node is None:
@@ -75,4 +86,4 @@ if password_node is None:
 else:
     password = password_node.value
     last_chain_value = reduce(hash_val_to_find, chain_length - 1)
-    print("The original password for this hash value is '{}': {} -- followed by the last chain value: {}".format(hash_val_to_find, password, last_chain_value))
+    print("The original password for this hash value is '{}': {} -- followed by the last chain value: {}".format(hash_val
